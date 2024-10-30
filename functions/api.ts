96d76196f566d0be503ba023a3be60114366ce6d
PHDfloppper <olivierbellerose1@gmail.com>
2024-10-30 16:01:57 -0400

    commit 2

---
+++ functions/api.ts
@@ -0,0 +1,15 @@
+/**
+ * Point d'entrée pour les fonctions de Netlify
+ */
+
+import app from "../src/server";
+import dotenv from 'dotenv';
+
+import serverless from 'serverless-http';
+
+// *** Variables d'environnement ***
+
+dotenv.config();
+
+// *** Le handler requis par Netlify **
+export const handler = serverless(app);
