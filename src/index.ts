646b9629aa6ff3b74107637553f0a9fb1f461b35
PHDfloppper <olivierbellerose1@gmail.com>
2024-11-01 09:43:17 -0400

    changement du lien de connexion pour mongodb atlas numéro2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 import { connect } from 'mongoose';
-
-
+import mongoose from 'mongoose';
 import EnvVars from '@src/common/EnvVars';
 import server from './server';
-
 
 // **** Run **** //
 
 const SERVER_START_MSG = ('Express server started on port: ' + 
   EnvVars.Port.toString());
 
+console.log(EnvVars.MongoDb_URI);
+  
 // Connecter à la base de données
 connect(EnvVars.MongoDb_URI)
   .then(() => server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG)))
